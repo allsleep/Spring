adce87c93c5514e42764d60560b8ea3cec8e4066
linhan <linhan1208@outlook.com>
2021-10-10 17:45:28 +0800

    win7

---
+++ helloworld/src/com/step1/helloWorld.java
@@ -0,0 +1,13 @@
+package com.step1;
+
+public class helloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage(){
+        System.out.println("message " + message);
+    }
+}
